39b299d8889165c0cb1d86dd1438c75e63f348ac
Dimitris Tsapnidis <dimitrios.tsapnidis@gmail.com>
2018-01-05 13:16:06 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='test_package',
+      version='0.1',
+      description='This is a testing package',
+      url='',
+      author='Mits Tsap',
+      author_email='mitstsap@example.com',
+      license='MIT',
+      packages=['test_package'],
+      zip_safe=False,
+      python_requires='>=3')
---
+++ test_package/joke.py
@@ -0,0 +1,2 @@
+def make_it():
+    print('Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja!')
